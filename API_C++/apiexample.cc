18bd7fc454d9f942bb3b3f2c6b69d557d1a06a48
wzuniga <wzuniga@episunsa.edu.pe>
2018-08-10 19:44:14 -0500

    Change Django to C++ API

--- API_C++/apiexample.cc
+++ API_C++/apiexample.cc
@@ -3,6 +3,7 @@
 #include <chrono>
 #include <thread>
 #include <string>
+#include <fstream>
 
 class RTreeAPI
 {
@@ -13,14 +14,16 @@ public:
         response.contentType("text/json");
         auto res = "{'1':[[[x,y],[x,y]]]}";
         response << res;
+        std::cout << "GET INSERT API "<< res <<std::endl;
     }
 
     void post(GloveHttpRequest& request, GloveHttpResponse& response)
     {
         response.contentType("text/json");
         //auto res = "{'1':[[[1,2],[1,2]]]}";
-        auto res = "{1:3}";
+        auto res = "{\"item\":3}";
         response << res;
+        std::cout << "POST INSERT API "<< res <<std::endl;
     }
 };
 
@@ -30,9 +33,19 @@ public:
     
     void get(GloveHttpRequest &request, GloveHttpResponse& response)
     {
-        response.contentType("text/json");
-        auto res = "html";
-        response << res;
+        response.contentType("text/html");
+        std::string line;
+        std::string file = "view/ViewTree.html";
+        if(!request.special["file"].empty())
+           file = "view/js/"+request.special["file"];
+        std::cout << "SEND "<< file <<std::endl;
+        std::ifstream myfile (file);
+        if (myfile.is_open()){
+            while ( getline (myfile,line) )
+                response << line << '\n';
+            myfile.close();
+        }
+        else response << "Unable to open file"; 
     }
 };
 
@@ -41,9 +54,9 @@ int main(int argc, char *argv[])
     RTreeAPI rtree;
     RTreeView view;
 
-  GloveHttpServer serv(8080, "", 2048);
+    GloveHttpServer serv(8080, "", 2048);
     namespace ph = std::placeholders;
-  /* serv.addRoute("/films/$filmId", restFilm, 2, 1, { "GET", "POST", "PUT", "PATCH", "DELETE" }); */
+    /* serv.addRoute("/films/$filmId", restFilm, 2, 1, { "GET", "POST", "PUT", "PATCH", "DELETE" }); */
     serv.addRest("/rtree", 1,
                              GloveHttpServer::jsonApiErrorCall,
                              std::bind(&RTreeView::get, &view, ph::_1, ph::_2)
@@ -53,13 +66,15 @@ int main(int argc, char *argv[])
                              std::bind(&RTreeAPI::get, &rtree, ph::_1, ph::_2),
                              std::bind(&RTreeAPI::post, &rtree, ph::_1, ph::_2)
     );
+    serv.addRest("/rtree/js/$file", 1,
+                             GloveHttpServer::jsonApiErrorCall,
+                             std::bind(&RTreeView::get, &view, ph::_1, ph::_2)
+    );
     
-  std::cout << "READY"<<std::endl;
-  while(1)
-    {
-      std::this_thread::sleep_for(std::chrono::seconds(1));
-    }
+    std::cout << "READY"<<std::endl;
+    while(1)
+        std::this_thread::sleep_for(std::chrono::seconds(1));
 
-  std::cout << "TEST"<<std::endl;
+    std::cout << "TEST"<<std::endl;
 
 }
--- r_tree/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-def getRegionMap(request):
-    print(request.method)
-    #print(request.data)
-    return render(request, 'r_tree/post_list.html', {})
